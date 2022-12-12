001e3f5b00ec30d6f177db32ec9a933952ea58eb
alezfi96 <alezfigueroa@hotmail.com>
2022-12-12 17:55:07 -0500

    ejercicio appenmultiplicar

---
+++ appendmultiplicr.py
@@ -0,0 +1,12 @@
+
+a=[]
+b=[]
+for i in range(6):
+    a.append(int(input("ingresa un numero a multiplicar ")))
+for i in a:
+    resultado=i*i
+    b.append(resultado)
+print("el resultado de la multiplicacion es",b)
+#
+
+
