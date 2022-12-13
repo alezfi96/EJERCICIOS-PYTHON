361e67ad06114089ea909ee0744cc3d808465930
alezfi96 <alezfigueroa@hotmail.com>
2022-12-12 22:21:11 -0500

    ejercicio

---
+++ ejercicio.py
@@ -0,0 +1,8 @@
+edad = input("como te llamas?")
+nombre = input("cuantos años tienes?")
+if edad<'25':
+ print("Eres estudiante")
+elif edad>'25':
+  print("eres profesor")
+elif edad<'65':
+  print("eres master")
