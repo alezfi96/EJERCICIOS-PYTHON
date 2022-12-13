40728066544f7f798579c21ee745dc6d879127a4
alezfi96 <alezfigueroa@hotmail.com>
2022-12-12 22:19:41 -0500

    ejercicio array

---
+++ comparararray.py
@@ -0,0 +1,7 @@
+nombre=['ferney']
+nombre2=[]
+nombre2.append((input("ingresa tu nombre ")))
+if nombre2 == nombre:
+    print("Los nombres son iguales")
+elif nombre2!=nombre:
+    print("Los nombres son differentes")
