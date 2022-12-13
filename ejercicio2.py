3cea8cb4a447752d4ac22e441ced6a4451645714
alezfi96 <alezfigueroa@hotmail.com>
2022-12-12 22:25:27 -0500

    ejerciciodos

---
+++ ejercicio2.py
@@ -0,0 +1,16 @@
+usuario=str(input("ingresa tu nombre"))
+num=True
+numero=55
+while num:
+ user = int(input("ingresa un numero")) 
+ if user < numero:
+     print("fallaste el numero es menor intenta nuevamente ") 
+ elif user>numero:
+    print("fallaste el numero es meyor intenta nuevamente")
+ elif user==numero:
+    print("felicidades lo lograste")
+    break
+
+
+  
+
